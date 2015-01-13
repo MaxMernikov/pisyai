79687fe685cadf20fcf6e722963a5e5862dee52e
Arty Schein <artem.schein@digitalizm.com>
2015-01-13 16:54:16 +0200

    добавил файлики ангуляра, добавил отсчет времени , подвинул чат наверх, мешает. думаю, что его можно вообще скрыть пока

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -6,12 +6,34 @@ function join_in_room(val){
   $('.js-join_in').append('<div class="list-group-item">'+ val +'</div>')
 }
 
+function countdown(callback) {
+	var bar = document.getElementById('progress'),
+    time = 0, max = 300,
+    int = setInterval(function() {
+        bar.style.width = Math.floor(100 * time++ / max) + '%';
+        if (time - 1 == max) {
+            clearInterval(int);
+            // 600ms - width animation time
+            callback && setTimeout(callback, 1000);
+        }
+    }, 100);
+};
+
 function start_game(val){
   $('.room h2').text(val['question']);
   $('.hallway, .welcome-page').hide();
   $('.room.answer').show();
+  countdown(function() {
+    alert('Redirect');
+    view_answers();
+  });
 }
 
+function view_answers(val) {
+	$('.room.answer').hide();
+	$('.room.choose-answer').show();
+	$('.room h2').text(val['question']);
+}
 $(document).ready(function(){
 	$('.form-horizontal .btn').on('click', function(){
 	    var well = $('.well.js-messages');
